92e1d49e71a62ad8506917e405b562dd071a3b5d
Aatishsinghshekhawat <aatishsinghdbs@gmail.com>
2025-11-12 17:39:38 +0530

    working on next Auth

--- my-app/src/app/api/auth/[...nextauth]/options.ts
+++ my-app/src/app/api/auth/[...nextauth]/options.ts
@@ -43,5 +43,27 @@ export const authOptions : NextAuthOptions = {
             }
         }
         })
-    ]
+    ],
+    callbacks : {
+    async session({ session, token }) {
+      return session
+    },
+    async jwt({ token, user }) {
+        if(user){
+            token._id = user._id?.toString()
+            token.isVerified = user.isVerified;
+            token.is
+        }
+
+      return token
+    }
+    },
+
+    pages : {
+        signIn: '/sign-in'
+    },
+    session: {
+        strategy: "jwt"
+    },
+    secret: process.env.NEXTAUTH_SECRET
 }
